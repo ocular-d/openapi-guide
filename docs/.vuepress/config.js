71c3af27aa83077f74387b4b7ac530fbe64e981b
sven <sven@so36.net>
2021-06-14 13:00:49 +0200

    vuepress(theme) update fonts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,25 @@ module.exports = {
   base: "/",
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
+    ['link', {
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i'
+  },],
+  ['link', {
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: 'https://fonts.googleapis.com/css?family=Miriam+Libre:400,700'
+  },],
   ],
   themeConfig: {
     searchPlaceholder: 'Search...',
+    editLinks: true,
+    editLinkText: 'Is this doc out of date? Suggest a change!',
     lastUpdated: 'Last Updated',
+    docsRepo: 'ocular-d/openapi-guide',
     docsDir: 'docs',
+    docsBranch: 'main',
     sidebar: [
       {
         title: 'Design Guide',
