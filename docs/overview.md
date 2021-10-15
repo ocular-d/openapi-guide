1bd30beed61e7487a75824eeb5343f347e2053cd
sven <sven@so36.net>
2021-10-15 16:46:59 +0200

    chore(setup) wip - new landing

--- docs/overview.md
+++ docs/overview.md
@@ -1,7 +1,7 @@
 ---
 id: overview
-title: Documentation
-sidebar_label: Welcome
+title: Classic UI
+sidebar_label: Overview
 description: Landing Page for Sauce Labs Documentation
 hide_table_of_contents: true
 ---
@@ -9,42 +9,42 @@ hide_table_of_contents: true
 <div className="box-wrapper" markdown="1">
   <div className="box box1 card">
     <div className="container">
-    <h2>Live Testing</h2>
-    <p>Browse through the Live Testing documentation to manually test your application on Sauce Labs Cloud.</p>
+    <h2>Theming</h2>
+    <p>All about theming</p>
     <ul>
-        <li><a href="/web-apps/live-testing/live-cross-browser-testing">Web Applications</a></li>
-        <li><a href="/mobile-apps/live-testing/live-mobile-app-testing/">Mobile Applications</a></li>
+        <li><a href="/web-apps/live-testing/live-cross-browser-testing">Diazo</a></li>
+        <li><a href="/mobile-apps/live-testing/live-mobile-app-testing/">Barceloneta</a></li>
     </ul>
     </div>
   </div>
   <div className="box box2 card">
     <div className="container">
-    <h2>Automated Testing</h2>
-    <p>Browse through the Automated Testing documentation to see examples of how to execute tests in your preferred automation framework.</p>
+    <h2>Registry</h2>
+    <p>All about the registry.</p>
     <ul>
-        <li><a href="/web-apps/automated-testing/selenium">Selenium</a> and <a href="/mobile-apps/automated-testing/appium">Appium</a></li>
-        <li><a href="/mobile-apps/automated-testing/espresso-xcuitest">Espresso and XCUITest</a></li>
-        <li><a href="/web-apps/automated-testing/cypress">Cypress</a>, <a href="/web-apps/automated-testing/puppeteer">Puppeteer</a>, <a href="/web-apps/automated-testing/testcafe">TestCafe</a>, or <a href="/web-apps/automated-testing/playwright">Playwright</a></li>
+        <li><a href="/web-apps/automated-testing/selenium">This</a> and <a href="/mobile-apps/automated-testing/appium">That</a></li>
+        <li><a href="/mobile-apps/automated-testing/espresso-xcuitest">Link 1</a></li>
+        <li><a href="/web-apps/automated-testing/cypress">Link 2</a>, <a href="/web-apps/automated-testing/puppeteer">cool</a>, <a href="/web-apps/automated-testing/testcafe">another link</a>, or <a href="/web-apps/automated-testing/playwright">Foobar</a></li>
     </ul>
     </div>
   </div>
   <div className="box box3 card">
     <div className="container">
-    <h2>Sauce Trusted Connection</h2>
-    <p>Browse the security documentation to learn how to communicate with Sauce Labs Cloud from your private network.</p>
+    <h2>Icons</h2>
+    <p>All about Icons.</p>
     <ul>
-        <li><a href="/secure-connections/sauce-connect">Sauce Connect Proxy</a></li>
-        <li><a href="/secure-connections/ipsec-vpn">IPSec VPN</a></li>
+        <li><a href="/secure-connections/sauce-connect">Link 1</a></li>
+        <li><a href="/secure-connections/ipsec-vpn">Link 2</a></li>
     </ul>
     </div>
   </div>
   <div className="box box4 card">
     <div className="container">
-    <h2>CI / CD</h2>
-    <p>Browse the continuous integration and continuous delivery documentation to explore how to integrate Sauce Labs into your DevOps pipeline.</p>
+    <h2>What is Plone Classic UI</h2>
+    <p>What is it, what is the difference to Plone Volto.</p>
     <ul>
-        <li><a href="/ci/jenkins">Jenkins</a></li>
-        <li><a href="/ci/bamboo">Bamboo</a></li>
+        <li><a href="/ci/jenkins">Differences</a></li>
+        <li><a href="/ci/bamboo">FAQ</a></li>
         <li>and <a href="/ci">More</a></li>
     </ul>
     </div>
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -3,13 +3,12 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import CodeBlock from '@theme/CodeBlock';
 import CodeBlockWithTitle from '../components/CodeBlockWithTitle'
-import EmailSignupForm from '../components/EmailSignupForm/index.js'
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import Head from '@docusaurus/Head';
 import styles from './styles.module.css';
-import Modal from 'react-modal';
+
 
 const features = [
   {
@@ -70,38 +69,6 @@ function PageBreakWithLogo() {
   )
 }
 
-function WaspLatestVersion() {
-  const [latestRelease, setLatestRelease] = useState(null)
-
-  useEffect(() => {
-    const fetchRelease = async () => {
-      const response = await fetch(
-        'https://api.github.com/repos/wasp-lang/wasp/releases'
-      )
-      console.log(response)
-      const releases = await response.json()
-      if (releases) {
-        setLatestRelease(releases[0])
-      }
-    }
-    fetchRelease()
-  }, [])
-
-  if (latestRelease) {
-    return (
-      <h4 className={styles.waspVersion}>
-        <a href={latestRelease.html_url}>
-          { latestRelease.name }
-        </a>
-      </h4>
-    )
-  } else {
-    return null
-  }
-}
-
-
-
 function HeroCodeExample() {
   // NOTE: There is an image in static/img/hero-code-shot.png of this code,
   //   used as the main image of the web app (specified via <meta>) when being
@@ -110,256 +77,28 @@ function HeroCodeExample() {
   // (just take a screenshot).
 
   const createAppWaspCode =
-`app todoApp {
-  title: "ToDo App" /* visible in tab */
-}
-
-route "/" -> page Main
-page Main {
-  /* import your React code */
-  component: import Main from "@ext/Main.js"
-}
-
-auth { /* full-stack auth out-of-the-box */
-  userEntity: User,
-  methods: [ EmailAndPassword ],
-}
+`paths:
+/users/{userId}:
+  get:
+    summary: Get a user by ID
+    parameters:
+      - in: path
+        name: userId
+        schema:
+          type: integer
+        required: true
+        description: Numeric ID of the user to get
 `
   return (
-    <CodeBlockWithTitle title="todoApp.wasp" language="css">
+    <CodeBlockWithTitle title="" language="css">
       { createAppWaspCode }
     </CodeBlockWithTitle>
-  )
-}
-
-function CodeExamples() {
-  const CodeExample = Object.freeze({
-    NEW_APP: 'Create a new app',
-    DEFINE_ENTITY: 'Define and query a data model',
-    ADD_AUTH: 'Add authentication'
-  })
-
-  const getButtonTextForCodeExample = (codeExample) => CodeExample[codeExample]
-
-  const [currentCodeExample, setCodeExample] = useState(CodeExample.NEW_APP)
-
-  function CurrentCodeExample() {
-    if (currentCodeExample === CodeExample.NEW_APP) {
-    const createAppWaspCode =
-`/* global app settings */
-app todoApp {
-  title: "ToDo App" /* browser tab title */
-}
-
-/* routing */
-route "/" -> page Main
-page Main {
-  component: import Main from "@ext/Main" /* import your React code */
-}
-`
-
-    const createAppMainComponentCode =
-`import React from 'react'
-
-export default () => <span> Hello World! </span>
-`
-      return (
-        <div className="codeExampleFiles">
-          <CodeBlockWithTitle title="todoApp.wasp" language="css">
-            { createAppWaspCode }
-          </CodeBlockWithTitle>
-
-          <CodeBlockWithTitle
-            title="ext/Main.js | External React code, imported above"
-            language="jsx">
-            { createAppMainComponentCode }
-          </CodeBlockWithTitle>
-
-          <div>
-            That's it, this is the whole app! Run <code>wasp start</code> and check it out
-            at <code>http://localhost:3000</code>!
-          </div>
-        </div>
-      )
-    } else if (currentCodeExample === CodeExample.ADD_AUTH) {
-    const exampleCode =
-`/* ... */
-
-/* full-stack auth out-of-the-box */
-auth {
-  userEntity: User,
-  methods: [ EmailAndPassword ], /* more methods coming soon */
-  onAuthFailedRedirectTo: "/login"
-}
-
-/* email & password required because of the auth method above */
-entity User {=psl
-    id          Int     @id @default(autoincrement())
-    email       String  @unique
-    password    String
-psl=}
-
-page Main {
-  authRequired: true, /* available only to logged in users */
-  component: import Main from "@ext/Main"
-}
-`
-
-      const mainUsingAuthCode =
-`import React from 'react'
-import Todo from './Todo.js'
-
-/* Because of authRequired property in todoApp.wasp, this page is
- * available only to logged in users and prop 'user' is automatically provided by wasp. */
-export default ({ user }) => {
-  return <Todo/>
-}
-`
-      return (
-        <div className="codeExampleFiles">
-          <CodeBlockWithTitle title="todoApp.wasp" language="css">
-            { exampleCode }
-          </CodeBlockWithTitle>
-          <CodeBlockWithTitle title="ext/Main.js | Checking if user is logged in" language="jsx">
-            { mainUsingAuthCode }
-          </CodeBlockWithTitle>
-
-          <div>
-            To learn more about authentication & authorization in Wasp, check
-            the <Link to={useBaseUrl('/docs/language/basic-elements#authentication--authorization')}>docs</Link>.
-          </div>
-        </div>
-      )
-    } else if (currentCodeExample === CodeExample.DEFINE_ENTITY) {
-      const defineEntityWaspCode =
-`/* ... */
-
-/* Data model is defined via Prisma Schema Language (PSL) */
-entity Task {=psl
-    id          Int     @id @default(autoincrement())
-    description String
-    isDone      Boolean @default(false)
-psl=}
-
-query getTasks {
-  fn: import { getTasks } from "@ext/queries.js", /* import Node.js function */
-  /* A list of entities this query uses - useful for automatic invalidation and refetching */
-  entities: [Task]
-}
-`
-      const getTasksQueryCode =
-`export const getTasks = async (args, context) => {
-  // Since we above declared this query is using Task, it is automatically injected in the
-  // context.
-  return context.entities.Task.findMany()
-}
-`
-      const todoUsingGetTasksCode =
-`import React from 'react'
-import { useQuery } from '@wasp/queries'
-import getTasks from '@wasp/queries/getTasks.js'
-
-export default () => {
-  // Standard useQuery syntax, Wasp provides a thin wrapper around it.
-  const { data: tasks } = useQuery(getTasks)
-  return <Tasks tasks={tasks}/>
-}
-`
-      return (
-        <div className="codeExampleFiles">
-          <CodeBlockWithTitle title="todoApp.wasp" language="css">
-            { defineEntityWaspCode }
-          </CodeBlockWithTitle>
-          <CodeBlockWithTitle
-            title="ext/queries.js | Node.js function imported in a query above"
-            language="jsx"
-          >
-            { getTasksQueryCode }
-          </CodeBlockWithTitle>
-          <CodeBlockWithTitle title="ext/Todo.js | Invoking query from React code" language="jsx">
-            { todoUsingGetTasksCode }
-          </CodeBlockWithTitle>
-
-          <div>
-            To learn more about working with data in Wasp, check
-            the <Link to={useBaseUrl('/docs/language/language/basic-elements#entity')}>docs</Link>.
-          </div>
-        </div>
-      )
-    } else {
-      console.log('this should never happen.')
-      return null
-    }
-  }
-
-  function Buttons() {
-
-    function Button({ codeExampleKey }) {
-      return (
-        <button
-          className={clsx('button',
-            'info', 
-            currentCodeExample === CodeExample[codeExampleKey] && 'is-active',
-            'button--secondary'
-          )}
-          onClick={() => setCodeExample(CodeExample[codeExampleKey])}
-        >
-          { getButtonTextForCodeExample(codeExampleKey) }
-        </button>
-      )
-    }
-
-    return (
-      Object.keys(CodeExample).map((k, idx) => <Button codeExampleKey={k} key={idx} />)
-    )
-  }
-
-  return (
-    <div className="row CodeExamples">
-      <div className="ButtonTabs col col--3">
-        <div>
-          <Buttons/>
-        </div>
-      </div>
-
-      <div className="col col--9">
-        <CurrentCodeExample/>
-      </div>
-    </div>
   )
 }
 
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
-
-  const [modalIsOpen, setModalIsOpen] = React.useState(false)
-
-  const todoTutorialUrl = useBaseUrl('docs/tutorials/todo-app');
-  const waspRwaDemoUrl = 'https://wasp-rwa.netlify.app';
-
-  const modalStyles = {
-    content : {
-      top                   : '40%',
-      left                  : '40%',
-      transform             : 'translate(-30%, -30%)',
-      paddingTop            : '4rem',
-      background            : 'var(--ifm-hero-background-color)',
-      /*
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      */
-    }
-  };
-
-  function openModal() {
-    setModalIsOpen(true);
-  }
-
-  function closeModal() {
-    setModalIsOpen(false);
-  }
 
   return (
     <Layout
@@ -386,9 +125,8 @@ function Home() {
                     in React, Node.js and Prisma.
                   </h3>
                   <div className="hero-works-with-icons">
-                    <img src="img/react-logo.svg" />
-                    <img src="img/nodejs-logo.svg" />
-                    <img src="img/prisma-logo.svg" />
+                    <img src="img/oas-logo.png" />
+
                   </div>
                 </div>
               </div>
@@ -399,52 +137,6 @@ function Home() {
 
             </div> {/* End of col. */}
 
-          </div> {/* End of row. */}
-
-          <div className={clsx('row', styles.responsiveCentered, styles.tryWaspRow)}>
-            <div className="col col--10 col--offset-1">
-
-              <Modal
-                isOpen={modalIsOpen}
-                style={modalStyles}
-                onRequestClose={closeModal}
-                shouldCloseOnOverlayClick={true}
-              >
-                <div className="container">
-                  <div className={clsx('row')}>
-                    <div className="col col--10">
-
-
-                      <h2 className="modal-step-title">1. Open your terminal and run:</h2>
-                      <div className={clsx(styles.startCliCmd)} style={{ height: '40px' }}>
-                        <span><code>curl -sSL https://get.wasp-lang.dev/installer.sh | sh</code></span>
-                      </div>
-
-                      <h2 className="modal-step-title "style={{marginTop: '4rem'}}>2. Create a new project:</h2>
-                      <div className={clsx(styles.startCliCmd)} style={{ height: '40px' }}>
-                        <span><code>wasp new MyFirstApp</code></span>
-                      </div>
-
-                      <h2 className="modal-step-title" style={{marginTop: '4rem'}}>3. Run your first app:</h2>
-                      <div className={clsx(styles.startCliCmd)} style={{ height: '40px', marginRight: '10px' }}>
-                        <span><code>cd MyFirstApp && wasp start</code></span>
-                      </div>
-                      <span>
-                        That's it!
-                        Open <Link to='http://localhost:3000/'>http://localhost:3000</Link> and see how it looks like!
-                      </span>
-
-                      <div style={{marginTop: '4rem'}}>
-                        <span>
-                          You ran into problems or want more details? Refer to the <Link to={useBaseUrl('/docs')}>docs</Link>.
-                        </span>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-              </Modal>
-            </div>
           </div> {/* End of row. */}
 
         </div>
